71636692a3986eeda75de9d0c7136871d1c14fd5
Bondarenko <antilooser@inbox.ru>
2013-02-28 02:39:30 +0200

    Adding of min searching algoritm

--- method.m
+++ method.m
@@ -1,4 +1,15 @@
 function [min] = method( f,a,b,delta, eps)
 
 while((b-a)/2>eps)
+	x1 = (a+b-delta)/2;
+	x2 = (a+b + delta)/2;
+	z = feval(f,x1);
+	n = feval (f,x2);
+	if(z < n)
+		b = x2;
+		
+	end
+	if(z > n)
+			a=x1;
+	end
 end
